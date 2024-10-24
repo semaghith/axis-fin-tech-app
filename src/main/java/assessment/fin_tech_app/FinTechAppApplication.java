a236c3576cc20a3f093ebbe616149b610b3838ca
Sema Ghith <semaghith11@gmail.com>
2024-10-25 01:53:48 +0300

    Create project

---
+++ src/main/java/assessment/fin_tech_app/FinTechAppApplication.java
@@ -0,0 +1,13 @@
+package assessment.fin_tech_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinTechAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinTechAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/assessment/fin_tech_app/FinTechAppApplicationTests.java
@@ -0,0 +1,13 @@
+package assessment.fin_tech_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinTechAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
